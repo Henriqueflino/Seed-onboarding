a442785f67b60cde0397de8d80f3d0931b8a17d5
Henrique Lino <74380067+Henriqueflino@users.noreply.github.com>
2022-05-25 08:24:20 -0300

    Atualização 25/05/2022
    adicionado ultimos arquivos nas branch

---
+++ Alura.Loja/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var contexto = new LojaContext())
+            {
+
+                var produtos = contexto.Produtos.ToList();
+                foreach (var p in produtos)
+                {
+                    Console.WriteLine(p);
+                }
+
+                Console.WriteLine("=================");
+                foreach (var e in contexto.ChangeTracker.Entries())
+                {
+                    Console.WriteLine(e);
+                }
+
+                var p1 = produtos.Last();
+                p1.Nome = "007 - O espião";
+
+                Console.WriteLine("=================");
+                foreach (var e in contexto.ChangeTracker.Entries())
+                {
+                    Console.WriteLine(e); 
+                }
+
+            }
+        }
+
+        
+    }
+}
